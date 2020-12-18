a512ac4878509463391ce78120e5634f595f5393
“teji1998” <tejasvinirpk@gmail.com>
2020-12-18 18:25:51 +0530

    Added:Welcome message

--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -6,7 +6,7 @@ namespace ProductReviewManagement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(" Welcome to product review management using LINQ !!!! ");
         }
     }
 }
