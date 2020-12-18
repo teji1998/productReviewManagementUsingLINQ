23f73e23fd66739755c591cee5b73c5857e2998a
“teji1998” <tejasvinirpk@gmail.com>
2020-12-18 18:57:14 +0530

    Added:POCO class for product review

---
+++ ProductReviewManagement/ProductReview.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductReviewManagement
+{
+    class ProductReview
+    {
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public int Rating { get; set; }
+        public string Review { get; set; }
+        public bool isLike { get; set; }
+
+        public string Tostring()
+        {
+            return "ProductId : " + ProductId + " UserId : " + UserId + " Rating : " + Rating + " Review : " + Review + "isLike : " + isLike ;
+        }
+    }
+}
