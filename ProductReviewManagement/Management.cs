cdf66ffb620ca39c2762cc73ecc3d0365ce07552
“teji1998” <tejasvinirpk@gmail.com>
2020-12-18 20:22:07 +0530

    Added:Code to get top three records based on rating

---
+++ ProductReviewManagement/Management.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace ProductReviewManagement
+{
+    public class Management
+    {
+        public readonly DataTable dataTable = new DataTable();
+
+        /// <summary>
+        /// Gets the top three records based on rating.
+        /// </summary>
+        /// <param name="reviewList">The review list.</param>
+        public void GetTopThreeRecordsBasedOnRating(List<ProductReview> reviewList)
+        {
+            var highestRatingRecord = (from productData in reviewList
+                                       orderby productData.Rating descending
+                                       select productData).Take(3);
+            Console.WriteLine("The top three records are : ");
+            foreach(var data in highestRatingRecord)
+            {
+                Console.WriteLine(data.ToString());
+            }
+
+        }
+    }
+}
--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -9,6 +9,7 @@ namespace ProductReviewManagement
         {
             Console.WriteLine("Welcome to product review management using LINQ !!!");
 
+            Management management = new Management();
             List<ProductReview> productReviewList = new List<ProductReview>()
             {
                 new ProductReview() { ProductId = 1, UserId = 1,  Rating = 4.2, Review = "Good", isLike = false },
@@ -18,13 +19,13 @@ namespace ProductReviewManagement
                 new ProductReview() { ProductId = 5, UserId = 3,  Rating = 4.2, Review = "nice", isLike = true },
                 new ProductReview() { ProductId = 6, UserId = 4,  Rating = 1, Review = "bad",  isLike = true },
                 new ProductReview() { ProductId = 7, UserId = 6,  Rating = 4.1, Review = "Good", isLike = false },
-                new ProductReview() { ProductId = 8, UserId = 5,  Rating = 5, Review = "nice", isLike = false },
+                new ProductReview() { ProductId = 8, UserId = 5,  Rating = 4.25, Review = "nice", isLike = false },
                 new ProductReview() { ProductId = 9, UserId = 8,  Rating = 5, Review = "nice", isLike = true },
                 new ProductReview() { ProductId = 16,UserId = 18, Rating = 3.6,Review = "nice", isLike = true },
                 new ProductReview() { ProductId = 3, UserId = 18, Rating = 1.5,Review = "bad", isLike = true },
-                new ProductReview() { ProductId = 10,UserId = 10, Rating = 5, Review = "nice", isLike = true },
-                new ProductReview() { ProductId = 12,UserId = 12, Rating = 6.4, Review = "nice", isLike = true },
-                new ProductReview() { ProductId = 13,UserId = 12, Rating = 7, Review = "Good", isLike = true },
+                new ProductReview() { ProductId = 10,UserId = 10, Rating = 3.75, Review = "nice", isLike = true },
+                new ProductReview() { ProductId = 12,UserId = 12, Rating = 3.64, Review = "nice", isLike = true },
+                new ProductReview() { ProductId = 13,UserId = 12, Rating = 4.97, Review = "Good", isLike = true },
                 new ProductReview() { ProductId = 14,UserId = 4,  Rating = 3.8, Review = "nice", isLike = true },
                 new ProductReview() { ProductId = 15,UserId = 14, Rating = 4.8, Review = "nice", isLike = true },
                 new ProductReview() { ProductId = 16,UserId = 18, Rating = 1.8, Review = "bad", isLike = true },
@@ -40,9 +41,14 @@ namespace ProductReviewManagement
                 new ProductReview() { ProductId = 13,UserId = 18, Rating = 3.95,Review = "nice", isLike = true },
             };
 
+            //to get values added in list
             foreach (var data in productReviewList) {
                 Console.WriteLine(data.ToString());
             }
+            
+            //to get top three records based on highest rating
+            management.GetTopThreeRecordsBasedOnRating(productReviewList);
+
         }
     }
 }
